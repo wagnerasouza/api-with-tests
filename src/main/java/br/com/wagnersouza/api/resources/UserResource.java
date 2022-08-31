fa70ce8f4b548ffddf83d6fbbc698f3f56984b3a
Wagner Souza <wg_antonio@hotmail.com>
2022-08-31 16:04:50 -0300

    Endpoint findAll para user.

--- src/main/java/br/com/wagnersouza/api/resources/UserResource.java
+++ src/main/java/br/com/wagnersouza/api/resources/UserResource.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
@@ -24,5 +27,13 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok().body(service.findAll()
+                .stream()
+                .map(u -> mapper.map(u, UserDTO.class))
+                .collect(Collectors.toList()));
     }
 }
--- src/main/java/br/com/wagnersouza/api/services/UserService.java
+++ src/main/java/br/com/wagnersouza/api/services/UserService.java
@@ -2,7 +2,11 @@ package br.com.wagnersouza.api.services;
 
 import br.com.wagnersouza.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
